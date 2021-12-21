ecd865cd8c4d397ce63fc63576f0ce9bed8c5597
Blake McCarty <blake_mccarty@yahoo.com>
2021-12-21 13:06:55 -0600

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const { ApolloServer } = require("appolo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typesDefs,
+  resolvers,
+});
 
-app.use(express.urlencoded({ extended: true }));
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port :${PORT}!`);
+    console.log(
+      `Use GraphQl at https://localhost:${PORT}${server.graphqlPath}`
+    );
+  });
 });
